06d85bcc75d4e1dcb1f17c629554fbdf05a37350
kobymallah <mallah@post.bgu.ac.il>
2026-01-23 17:56:22 +0200

    Cleaned up and changed icon

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,8 @@
 import { createEnv } from '@t3-oss/env-core'
-import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    SERVER_URL: z.string().url().optional(),
+    
   },
 
   /**
@@ -13,7 +12,7 @@ export const env = createEnv({
   clientPrefix: 'VITE_',
 
   client: {
-    VITE_APP_TITLE: z.string().min(1).optional(),
+    
   },
 
   /**
